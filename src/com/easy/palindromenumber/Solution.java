2815ee8b4af65bbb0266d91497c444b83fd78225
chenlangping <328566090@qq.com>
2018-07-30 21:28:33 +0800

    新增009

---
+++ src/com/easy/palindromenumber/Solution.java
@@ -0,0 +1,32 @@
+package com.easy.palindromenumber;
+
+public class Solution {
+    public boolean isPalindrome(int x) {
+        try{
+            if(x==0){
+                return true;
+            }else if(x<0){
+                return false;
+            }else {
+                String num = String.valueOf(x);
+                String mnum = new StringBuffer(num).reverse().toString();
+                if (num.equals(mnum)){
+                    return true;
+                }else {
+                    return false;
+                }
+            }
+        }catch (Exception e){
+            return false;
+        }
+
+    }
+
+    /*
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        int x = 10;
+        System.out.print(solution.isPalindrome(x));
+    }
+    */
+}

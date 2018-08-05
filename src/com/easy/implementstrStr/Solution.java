cd19e18d7e937f8cd94c6b3c816b558c0dba6590
chenlangping <328566090@qq.com>
2018-08-05 10:34:12 +0800

    新增028

---
+++ src/com/easy/implementstrStr/Solution.java
@@ -0,0 +1,37 @@
+package com.easy.implementstrStr;
+
+public class Solution {
+    public int strStr(String haystack, String needle) {
+        if (needle.equals("") || null == needle) {
+            return 0;
+        }
+        for(int i=0;i<=haystack.length() - needle.length();i++){
+            int now = i; //记录目前的haystack中的索引
+            int count = 0; //匹配上的个数
+            for (int j = 0 ;j<needle.length();j++){
+                if(haystack.charAt(now)!=needle.charAt(j)){
+                    break;
+                }else {
+                    count++;
+                    now++;
+                }
+                if(count == needle.length()){
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+
+
+    /*
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String haystack = "hello";
+        String needle = "ll";
+        System.out.println(solution.strStr(haystack,needle));
+
+    }
+    */
+
+}

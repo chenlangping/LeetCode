78d98df7a008dafbce84809443e812caa0350974
chenlangping <328566090@qq.com>
2018-08-14 10:51:46 +0800

    优化016 version 0.2

--- src/com/medium/threeSumClosest/Solution.java
+++ src/com/medium/threeSumClosest/Solution.java
@@ -8,14 +8,24 @@ public class Solution {
         int minError = Math.abs(target - nums[0] - nums[1] - nums[2]);
         int result = nums[0] + nums[1] + nums[2];
         for (int i = 0; i < nums.length - 2; i++) {
-            for (int j = i + 1; j < nums.length - 1; j++) {
-                for (int k = j + 1; k < nums.length; k++) {
-                    int curSum = nums[i] + nums[j] + nums[k];
-                    if (Math.abs(target - curSum) < Math.abs(minError)) {
-                        minError = Math.abs(target - curSum);
-                        result = curSum;
+            int small = i + 1;
+            int big = nums.length - 1;
+            while (big > small) {
+                int curSum = nums[i] + nums[big] + nums[small];
+                if(Math.abs(target - curSum) < minError){
+                    minError = Math.abs(target-curSum);
+                    result = curSum;
+
+                    //优化操作
+                    while(big > small &&nums[small]==nums[small+1]){
+                        small++;
                     }
-                }
+                    while(big > small &&nums[big]==nums[big-1]){
+                        big--;
+                    }
+                }else if(curSum < target){
+                    small++;
+                }else big--;
             }
         }
         return result;
@@ -23,8 +33,8 @@ public class Solution {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int nums[] = {0,0,0};
-        int target = 1;
+        int nums[] = {1,2,5,10,11};
+        int target = 12;
         System.out.println(solution.threeSumClosest(nums, target));
     }
 }

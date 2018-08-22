5343de2e58f64dd439c5910baf33e86e5dd99c04
chenlangping <328566090@qq.com>
2018-08-22 10:57:17 +0800

    修改算法，但是证明是错误的

--- src/com/hard/trappingRainWater/Solution.java
+++ src/com/hard/trappingRainWater/Solution.java
@@ -6,31 +6,13 @@ import java.util.List;
 
 public class Solution {
     public int trap(int[] height) {
-        List<Integer> list = new ArrayList<>();
-        int start = 0;
-        while (-1 != (start = findHighest(height, start))) {
-            list.add(start);
-            start++;
-        }
-
-
-        System.out.println(list);
-        int[] array = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            array[i] = list.get(i);
-        }
         int result = 0;
-        int left = 0;
-        int right = 0;
-        for (int i = 0; i < array.length - 1; i++) {
-            if (array[i] + 1 == array[i + 1]) {
-                left = i;
-                continue;
-            } else {
-                //计算
-                right = i + 1;
-                result += calculateTwoColumn(height, array[left], array[right]);
-            }
+        int size = height.length;
+        for (int i = 1; i < size - 1; i++) {
+            int left_max = 0;
+            int right_max = 0;
+
+
         }
         return result;
     }
@@ -51,25 +33,56 @@ public class Solution {
         int high = Math.min(height[start], height[end]);
         int exist = 0;
         for (int i = start + 1; i < end; i++) {
-            exist += height[i];
+            if(high>=height[i]){
+                exist += height[i];
+            }else {
+                exist+=high;
+            }
         }
+        System.out.println("wid=" + wid);
+        System.out.println("high=" + high);
+        System.out.println("exist=" + exist);
         return wid * high - exist;
     }
 
     public int trap2(int[] height) {
         int result = 0;
         int size = height.length;
-        for (int i = 1; i < size - 1; i++) {
+        int left = 0;
+        int right = 0;
+        int start = 0;
+
+        //第一步，找到一根局部最高的
+        for (int i = 0; i < size - 2; i++) {
+            if (height[i]>height[i+1]){
+                start = i;
+                break;
+            }
+        }
+        System.out.println("start = "+start );
+        for (int i = start +1; i < size - 1; i++) {
             int max_left = 0, max_right = 0;
             for (int j = i; j >= 0; j--) { //Search the left part for max bar size
-                max_left = Math.max(max_left, height[j]);
+                //max_left = Math.max(max_left, height[j]);
+                if (height[j] > max_left) {
+                    max_left = height[j];
+                    left = j;
+                }
             }
             for (int j = i; j < size; j++) { //Search the right part for max bar size
-                max_right = Math.max(max_right, height[j]);
+                //max_right = Math.max(max_right, height[j]);
+                if (height[j] > max_right) {
+                    max_right = height[j];
+                    right = j;
+                }
             }
 
-            //System.out.println(height[i] + "左边最大的数=" + max_left + "  右边最大的数=" + max_right);
-            result += Math.min(max_left, max_right) - height[i];
+            System.out.println(height[i] + "左边最大的数=" + max_left + "  右边最大的数=" + max_right);
+            result += calculateTwoColumn(height, left, right);
+            System.out.println("result = "+result);
+            i = right;
+            System.out.println("i="+i);
+            //result += Math.min(max_left, max_right) - height[i];
         }
         return result;
     }
@@ -121,10 +134,10 @@ public class Solution {
         int[] height = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1, 6, 7, 8, 4, 5, 6, 0};
         //int[] height = {5, 4, 3, 4, 5, 3, 2};
         //int[] height = {3, 2, 1, 2, 1, 6, 7, 8, 4, 5, 6, 0};
-        //int[] height = {4,2,1,3,1,6,7,8,4,5,6};
+        //int[] height = {4, 2, 1, 3, 1, 6, 7, 8, 4, 5, 6};
 
-        System.out.println(solution.trap3(height));
+        System.out.println(solution.trap2(height));
 
-        System.out.println("正确答案：" + solution.trap2(height));
+        System.out.println("正确答案：" + solution.trap3(height));
     }
 }

9ade69ce1c3c69f98e76039d4a8443ee5b3474da
chenlangping <328566090@qq.com>
2018-08-23 15:26:00 +0800

    新增HashSet方法

--- src/com/easy/jewelsAndStones/Solution.java
+++ src/com/easy/jewelsAndStones/Solution.java
@@ -1,5 +1,8 @@
 package com.easy.jewelsAndStones;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Solution {
     public int numJewelsInStones(String J, String S) {
         int sum = 0;
@@ -17,10 +20,24 @@ public class Solution {
         return S.replaceAll("[^" + J + "]", "").length();
     }
 
+    public int numJewelsInStones3(String J, String S) {
+        Set set = new HashSet();
+        int sum = 0;
+        for (char j : J.toCharArray()) {
+            set.add(j);
+        }
+        for (char s : S.toCharArray()) {
+            if (set.contains(s)) {
+                sum++;
+            }
+        }
+        return sum;
+    }
+
     public static void main(String[] args) {
-        //String J = "aA", S = "aAAbbbb";
-        String J = "z", S = "ZZ";
+        String J = "aA", S = "aAAbbbb";
+        //String J = "z", S = "ZZ";
         Solution s = new Solution();
-        System.out.println(s.numJewelsInStones(J, S));
+        System.out.println(s.numJewelsInStones3(J, S));
     }
 }

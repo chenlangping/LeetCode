94c32a25690fb6f8b6cf5ff8e99f6ff15bc03fe0
chenlangping <328566090@qq.com>
2018-09-04 23:59:00 +0800

    新增035

--- README.md
+++ README.md
@@ -13,7 +13,7 @@
 * [2018年8月5日     028<br>](https://www.jianshu.com/p/f37050dc0e20)
 * [2018年8月5日     011<br>](https://www.jianshu.com/p/4d38427b73db)
 * [2018年8月5日     073<br>](https://www.jianshu.com/p/016ee54b29d9)
-* 2018年8月6日     035<br>
+* [2018年8月6日     035<br>](https://www.jianshu.com/p/0cfbbc37106f)
 * 2018年8月7日     038<br>
 * 2018年8月8日     002<br>
 * 2018年8月9日     004<br>
--- src/com/easy/searchInsertPosition/Solution.java
+++ src/com/easy/searchInsertPosition/Solution.java
@@ -4,25 +4,26 @@ public class Solution {
     public int searchInsert(int[] nums, int target) {
         //二分查找法
         int small = 0;
-        int big = nums.length-1;
-        int medium = -1;
-        while(big>=small){
-            medium = (small+big)/2;
-            if(target == nums[medium]){
+        int big = nums.length - 1;
+        int medium;
+        while (big >= small) {
+            medium = (small + big) / 2;
+            if (target == nums[medium]) {
                 return medium;
-            }else if(target > nums[medium]){
-                small = medium+1;
-            }else {
-                big = medium-1;
+            } else if (target > nums[medium]) {
+                small = medium + 1;
+            } else {
+                big = medium - 1;
             }
         }
-        return nums[medium]>target?medium:medium+1;
+        //return nums[medium] > target ? medium : medium + 1;
+        return small;
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int []nums = {1,3,5,6};
+        int[] nums = {1, 3, 5, 6};
         int target = 0;
-        System.out.println(solution.searchInsert(nums,target));
+        System.out.println(solution.searchInsert(nums, target));
     }
 }

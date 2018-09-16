9804eb908bf687ddbb57dd1b714ecdb74d2ff2db
chenlangping <328566090@qq.com>
2018-09-16 11:59:45 +0800

    新增058

--- src/com/easy/lengthOfLastWord/Solution.java
+++ src/com/easy/lengthOfLastWord/Solution.java
@@ -2,16 +2,21 @@ package com.easy.lengthOfLastWord;
 
 public class Solution {
     public int lengthOfLastWord(String s) {
+        //特殊情况处理
         if (s.length() == 0) {
             return 0;
         }
         int result = 0;
         int i = s.length() - 1;
         int j = 0;
+
+        //对最后的空格进行记录
         while (i >= 0 && s.charAt(i) == ' ') {
             i--;
             j++;
         }
+
+        //计算最后一个单词的长度
         while (i >= 0) {
             if (s.charAt(i) == ' ') {
                 result = s.length() - i - 1 - j;
@@ -24,5 +29,9 @@ public class Solution {
             i--;
         }
         return result;
+    }
+
+    public int lengthOfLastWord2(String s) {
+        return s.trim().length() - s.trim().lastIndexOf(" ") - 1;
     }
 }

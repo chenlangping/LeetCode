c993c6cab60d6c902b079d09dce50e4cb48df093
chenlangping <328566090@qq.com>
2018-09-21 22:37:02 +0800

    修改136注释

--- src/com/easy/singleNumber/Solution.java
+++ src/com/easy/singleNumber/Solution.java
@@ -2,7 +2,7 @@ package com.easy.singleNumber;
 
 public class Solution {
     public int singleNumber(int[] nums) {
-        //抑或操作会留下最后的那一个数
+        //异或操作会留下最后的那一个数
         int result = nums[0];
         for (int i = 1; i < nums.length; i++) {
             result ^= nums[i];

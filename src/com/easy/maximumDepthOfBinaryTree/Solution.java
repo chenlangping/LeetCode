0ca462a099c8ea3d5732822e761c7cf13cbe1f8c
chenlangping <328566090@qq.com>
2018-10-09 18:05:58 +0800

    格式化代码

---
+++ src/com/easy/maximumDepthOfBinaryTree/Solution.java
@@ -0,0 +1,15 @@
+package com.easy.maximumDepthOfBinaryTree;
+
+import com.structure.TreeNode.TreeNode;
+
+public class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else {
+            int left = maxDepth(root.left);
+            int right = maxDepth(root.right);
+            return Math.max(left, right) + 1;
+        }
+    }
+}

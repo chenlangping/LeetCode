57ad9467501d0421c3424ced8a0a77c506f6b27c
chenlangping <328566090@qq.com>
2018-10-11 23:07:03 +0800

    新增240

---
+++ src/com/medium/searchA2DMatrixII/Solution.java
@@ -0,0 +1,23 @@
+package com.medium.searchA2DMatrixII;
+
+public class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int i = 0, j = n - 1;
+        while (i < m && j >= 0) {
+            if (matrix[i][j] == target) {
+                return true;
+            } else if (matrix[i][j] < target) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return false;
+    }
+
+}

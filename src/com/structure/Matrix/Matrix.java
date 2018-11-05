827daa0429c652e16ad41859ea3f9001eca6819b
chenlangping <328566090@qq.com>
2018-11-05 20:52:48 +0800

    新增打印方法重载

--- src/com/structure/Matrix/Matrix.java
+++ src/com/structure/Matrix/Matrix.java
@@ -23,6 +23,19 @@ public class Matrix {
         }
     }
 
+    public static void print(boolean[][] matrix) {
+        int rows = matrix.length;//行数
+        int columns = matrix[0].length;//列数
+        for (int i = 0; i < rows; i++) {
+            System.out.print("|\t");
+            for (int j = 0; j < columns; j++) {
+                System.out.print(matrix[i][j] + "\t");
+            }
+            System.out.print("|");
+            System.out.println();
+        }
+    }
+
     /**
      * 将矩阵按照对角线进行交换
      *

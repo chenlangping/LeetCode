c20830e1e7b80794517094b8128b0cfb3cba58ba
chenlangping <328566090@qq.com>
2018-12-28 20:12:13 +0800

    add 044

---
+++ src/com/easy/validAnagram/Solution.java
@@ -0,0 +1,4 @@
+package com.easy.validAnagram;
+
+public class Solution {
+}
--- src/com/hard/wildcardMatching/Solution.java
+++ src/com/hard/wildcardMatching/Solution.java
@@ -1,5 +1,7 @@
 package com.hard.wildcardMatching;
 
+import com.structure.Matrix.Matrix;
+
 public class Solution {
     public boolean isMatch(String s, String p) {
         boolean[][] match = new boolean[s.length() + 1][p.length() + 1];
@@ -10,6 +12,10 @@ public class Solution {
             else
                 match[s.length()][i] = true;
         }
+
+        Matrix.print(match);
+        System.out.println();
+
         for (int i = s.length() - 1; i >= 0; i--) {
             for (int j = p.length() - 1; j >= 0; j--) {
                 if (s.charAt(i) == p.charAt(j) || p.charAt(j) == '?')
@@ -20,6 +26,16 @@ public class Solution {
                     match[i][j] = false;
             }
         }
+
+        Matrix.print(match);
+
         return match[0][0];
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String s = "adcebeeeeee";
+        String p = "*a*";
+        System.out.println(solution.isMatch(s, p));
     }
 }

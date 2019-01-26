2b36485111d96ae2f54dfb6d8f10d4d8e54529a5
chenlangping <328566090@qq.com>
2019-01-26 20:26:34 +0800

    新增096

---
+++ src/com/medium/uniqueBinarySearchTrees/Solution.java
@@ -0,0 +1,18 @@
+package com.medium.uniqueBinarySearchTrees;
+
+public class Solution {
+    public int numTrees(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int level = 2; level <= n; level++)
+            for (int root = 1; root <= level; root++)
+                dp[level] += dp[level - root] * dp[root - 1];
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.numTrees(10));
+    }
+}

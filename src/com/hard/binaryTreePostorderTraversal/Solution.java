db6977402eeda88159d66989a087a403d13e77fe
chenlangping <328566090@qq.com>
2019-03-27 12:00:11 +0800

    add 145

--- src/com/hard/binaryTreePostorderTraversal/Solution.java
+++ src/com/hard/binaryTreePostorderTraversal/Solution.java
@@ -1,4 +1,33 @@
 package com.hard.binaryTreePostorderTraversal;
 
+import com.structure.TreeNode.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        helper(root, res);
+        return res;
+    }
+
+    private void helper(TreeNode root, List<Integer> res) {
+        if (root == null) {
+            return;
+        }
+        if (root.left!=null){
+            helper(root.left,res);
+        }
+        if (root.right!=null){
+            helper(root.right,res);
+        }
+        res.add(root.val);
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        TreeNode root = TreeNode.createTestData("[1,2,3,4,5,6,7,8]");
+        System.out.println(solution.postorderTraversal(root));
+    }
 }

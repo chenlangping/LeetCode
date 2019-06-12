e39a7ae5354d10a0693361bec1920694e559bf58
chenlangping <328566090@qq.com>
2019-06-12 23:39:08 +0800

    add 521

---
+++ src/com/easy/longestUncommonSubsequenceI/Solution.java
@@ -0,0 +1,11 @@
+package com.easy.longestUncommonSubsequenceI;
+
+public class Solution {
+    public int findLUSlength(String a, String b) {
+        if (a.equals(b)) {
+            return -1;
+        } else {
+            return Math.max(a.length(), b.length());
+        }
+    }
+}
